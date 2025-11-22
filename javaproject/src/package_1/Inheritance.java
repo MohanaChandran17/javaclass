5249f51d9fd014ecab24aad5da122aed44616e5c
abdmo <abdmohanabd17@gmail.com>
2025-11-23 00:12:06 +0530

    inheritance class added

---
+++ javaproject/src/package_1/Inheritance.java
@@ -0,0 +1,15 @@
+package package_1;
+ class dad
+ {
+	 int money = 2000;
+ }
+public class Inheritance extends dad
+{
+	public static void main (String args[])
+	{
+		Inheritance obj1=new Inheritance();
+		System.out.println(obj1.money);
+		
+	}
+
+}
