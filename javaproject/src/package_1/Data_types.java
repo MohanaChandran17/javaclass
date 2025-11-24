8826cf94a468494926b9403bdfffb845141f0c5c
abdmo <abdmohanabd17@gmail.com>
2025-11-24 16:56:34 +0530

    today java class

---
+++ javaproject/src/package_1/Data_types.java
@@ -0,0 +1,30 @@
+package package_1;
+
+public class Data_types
+{
+	public static void main(String args[])
+	{
+		
+	
+	byte b=10;
+	short a=1000;
+	int i=50000;
+	long l=100000L;
+	 
+	float f=5.75f;
+	double d=19.99;
+	char c='a';
+	boolean isbool=true;
+	
+	System.out.println("byte"+b);
+	System.out.println("short"+a);
+	System.out.println("int"+i);
+	System.out.println("long"+l);
+	System.out.println("float"+f);
+	System.out.println("double"+d);
+	System.out.println("char"+c);
+	System.out.println("boolean"+isbool);
+	
+	}
+
+}
---
+++ javaproject/src/package_1/NonPrimitiveDatatypes.java
@@ -0,0 +1,19 @@
+package package_1;
+
+public class NonPrimitiveDatatypes
+{
+	public static void main(String args[])
+	{
+		float [] f= {1.1f , 10.10f,11.11f,12.12f};
+		System.out.println("the array is :"+f[2]);
+		
+		double[] d= {19.20,20.00,34.983,54.947};
+		System.out.println("the array is "+d[3]);
+		
+		long[] l= {10000l,20000l,30000l,40000l,50000l,60000L,70000l};
+		System.out.println(l[3]);
+		
+	}
+	
+
+}
