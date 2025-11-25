37a92499a3a14ee8469c70e3b0482d656265b31c
abdmo <abdmohanabd17@gmail.com>
2025-11-25 23:18:18 +0530

    lknl

---
+++ javaproject/src/package_1/program2.java
@@ -0,0 +1,41 @@
+package package_1;
+
+import java.util.Scanner;
+
+public class program2
+{
+	public static void main(String args[])
+	{
+		Scanner scan=new Scanner(System.in);
+		int i=scan.nextInt();
+		System.out.println("enter int value");
+		
+		Scanner scan1=new Scanner(System.in);
+		float f=scan1.nextInt();
+		System.out.println("enter float value");
+		
+		Scanner scan2=new Scanner(System.in);
+		char c=scan2.next().charAt(0);
+		System.out.println("enter char value");
+		
+		Scanner scan3=new Scanner(System.in);
+		double d=scan3.nextDouble();
+		System.out.println("enter double value");
+		
+		Scanner scan4=new Scanner(System.in);
+		byte b=scan4.nextByte();
+		System.out.println("enter byte value");
+		
+		Scanner scan5=new Scanner(System.in);
+		short s=scan5.nextShort();
+		System.out.println("enter short value");
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
