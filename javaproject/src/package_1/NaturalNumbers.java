fde0081836eeaec7c655869ce3646abb4c535119
abdmo <abdmohanabd17@gmail.com>
2025-12-06 23:25:23 +0530

    hjava

---
+++ javaproject/src/package_1/NaturalNumbers.java
@@ -0,0 +1,26 @@
+package package_1;
+
+public class NaturalNumbers
+{
+
+	public static void main(String[] args)
+	{
+		int number=0;
+		for(int i=1;i<=10;i++)
+			number=number+i;
+		{
+		
+			
+		
+				System.out.println("sum of 10 natural numbrs is: "+number);
+			
+			
+			
+			
+		}
+		
+		
+        
+	}
+
+}
---
+++ javaproject/src/package_1/Question9.java
@@ -0,0 +1,27 @@
+package package_1;
+class StringTools
+{
+	
+
+
+String reverseword(String sentence)
+{
+	
+}
+
+
+
+public class Question9
+{
+
+
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
+
