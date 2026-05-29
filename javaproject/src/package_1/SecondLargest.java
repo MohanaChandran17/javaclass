e2f71b5f425dfd658c40abccd7184e6a3c81f520
abdmo <abdmohanabd17@gmail.com>
2026-05-29 23:37:27 +0530

    SecondLargest in array

---
+++ javaproject/src/package_1/SecondLargest.java
@@ -0,0 +1,25 @@
+package package_1;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 8, 15};
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("No second largest element found.");
+        } else {
+            System.out.println("Second largest element: " + secondLargest);
+        }
+    }
+}
